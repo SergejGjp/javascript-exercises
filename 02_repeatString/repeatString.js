8eae808cb2bf592166cab6b9b31f6d83046cb218
Sergej <gorbatschewsergej@gmail.com>
2023-09-30 21:57:29 +0900

    Clear Exercise 01 and 02 Solved the first two exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+        text += string;
+        };
+    } else if (num < 0) {
+        text = "ERROR";
+    };
+    return text;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
