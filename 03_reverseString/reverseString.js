4cb177ebe16a433ebe8060d3cd1689658dfe8505
Sergej <gorbatschewsergej@gmail.com>
2023-09-30 22:26:44 +0900

    Solve Exercise 03 Solved task 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = string;
+    let array = text.split("");
+    let reverse = array.reverse();
+    let reverseString = reverse.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
