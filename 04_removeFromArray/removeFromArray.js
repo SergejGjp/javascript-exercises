d98e0e981722b9cae461c20a30a4a58d5952870e
Sergej <gorbatschewsergej@gmail.com>
2023-10-01 22:33:11 +0900

    Solve Task 04. Solved exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...theArgs) {
+    for (const arg of theArgs) {
+        if (input.includes(arg)) {
+            let whereToRemove = input.indexOf(arg);
+            input.splice(whereToRemove, 1);
+        };
+    };
+    return input;
 };
 
 // Do not edit below this line
