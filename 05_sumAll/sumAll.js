e65c6a3ac54cd64dffb2cff109621ab98556ace3
Sergej <gorbatschewsergej@gmail.com>
2023-10-01 23:21:58 +0900

    Solve task 05. Solved exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        if (a > b) {
+            for (let i = a; i >= b; i--) {
+                sum += i;
+            };
+        } else {
+            for (let i = a; i <= b; i++) {
+                sum += i;
+            };
+        };
+    } else return "ERROR";
+    return sum;
 };
+    
 
 // Do not edit below this line
 module.exports = sumAll;
