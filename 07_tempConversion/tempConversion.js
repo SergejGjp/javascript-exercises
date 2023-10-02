810618e4ccb1e59ceeb91dde0f259a4928d0ed42
Sergej <gorbatschewsergej@gmail.com>
2023-10-02 22:40:24 +0900

    Solve task 07. Sovled exercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fahrConv =  (fahrenheit - 32) * (5/9) * 10;
+  fahrConv = Math.round(fahrConv) / 10;
+  return fahrConv;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let celConv = (celsius * 9/5 + 32) * 10;
+  celConv = Math.round(celConv) / 10;
+  return celConv;
 };
 
 // Do not edit below this line
