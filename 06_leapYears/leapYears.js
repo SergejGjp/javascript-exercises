fd0995bbf63c03b9a2521808dba421688338e26a
Sergej <gorbatschewsergej@gmail.com>
2023-10-02 23:05:50 +0900

    Add alternative solution to task 06. Made exercise 06 shorter.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,15 @@
+// const leapYears = function(year) {
+//     if (year % 100 == 0) {
+//         if (year % 400 == 0) {
+//             return true;
+//         } else return false;
+//     } else if (year % 4 == 0) {
+//         return true;
+//     } else return false;
+// };
+
 const leapYears = function(year) {
-    if (year % 100 == 0) {
-        if (year % 400 == 0) {
-            return true;
-        } else return false;
-    } else if (year % 4 == 0) {
+    if (year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)) {
         return true;
     } else return false;
 };
