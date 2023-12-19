7d360c688715ac7301f4d3c19406614cebbaf068
Sergej <gorbatschewsergej@gmail.com>
2023-12-19 21:09:05 +0900

    Solve 08_calculator task Solved the calculator tasks.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((last, item) => add(item, last), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (last, item) => last * item)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	let result = 1;
+  if (num === 0) {
+    return result;
+  } else { 
+    for (let i = 1; i<= num; i++) {
+      result*= i;
+  };
+  return result;
+}};
 
 // Do not edit below this line
 module.exports = {
