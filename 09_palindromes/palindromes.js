2b2d8242abf48d796ca7abf7da468a591e8ffe86
Sergej <gorbatschewsergej@gmail.com>
2023-12-20 22:08:27 +0900

    Solve 09.palindromes solved task no.9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = /[\.,?!]/g;
+    let newStr = str.replace(punctuation, "");
+    let reverseStr = newStr
+        .toLowerCase()
+        .split(" ")
+        .map(item => item.split("").reverse().join(""))
+        .reverse()
+        .join("");
+    let jointStr = newStr
+        .toLowerCase()
+        .split(" ")
+        .join("");
+    return reverseStr === jointStr;
 };
 
 // Do not edit below this line
