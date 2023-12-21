40d0afb9e38399f03097fe866b891443058c3e70
Sergej <gorbatschewsergej@gmail.com>
2023-12-21 22:56:20 +0900

    Solve 10_fibonacci Solved task no. 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    };
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        if (i === 0) {
+            arr.push(1);
+        } else if (i === 1) {
+            arr.push(1);
+        } else if (i >= 2) {
+            arr.push(arr[i - 1] + arr[i - 2]);
+        };
+        if (i === num -1){
+            return arr[num - 1];
+        };
+    };
 };
 
 // Do not edit below this line
