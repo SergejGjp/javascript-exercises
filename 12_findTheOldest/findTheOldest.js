0400629424807883009907edb2c262e97f3f6290
Sergej <gorbatschewsergej@gmail.com>
2023-12-22 23:26:49 +0900

    Solve task 12_findTheOldest Solved task no. 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) { 
+    arr.map( item => {
+        if (!item.yearOfDeath) {
+            item.yearOfDeath = new Date().getFullYear()
+        };
+    });
+    arr.sort((a, b) => 
+        (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    );
+    console.log(arr);
+    return arr[0];
 };
 
 // Do not edit below this line
